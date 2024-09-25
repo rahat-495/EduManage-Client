414b9012fbf4abbfc6e7310b4f61856eaf8fae8e
rahat-495 <md8702687@gmail.com>
2024-09-25 21:01:09 +0600

    class mates route h1 are added !

--- src/Pages/ClassMates/ClassMates.jsx
+++ src/Pages/ClassMates/ClassMates.jsx
@@ -1,7 +1,13 @@
 
 const ClassMates = () => {
     return (
-        <div className="min-h-[70vh]">
+        <div className="min-h-[70vh] flex flex-col items-center gap-5">
+
+            <h1 className="text-white text-4xl play">Class Mates</h1>
+
+            <div className="">
+                
+            </div>
             
         </div>
     );
